a1c250e4c42d96fc78c3e0e825708a452436c0bc
dp <david.penca111@gmail.com>
2019-11-07 03:12:20 +0100

    Need to call s3 from api.

--- services/src/main/java/si/fri/rso/fileStorage/services/fileStorageBean.java
+++ services/src/main/java/si/fri/rso/fileStorage/services/fileStorageBean.java
@@ -1,17 +1,71 @@
 package si.fri.rso.fileStorage.services;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.annotation.PostConstruct;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @ApplicationScoped
 public class fileStorageBean {
-    @PostConstruct
-    private void init() {
-        System.out.println("Users service");
+
+    AWSCredentials credentials = new BasicAWSCredentials(
+            "AKIAZRMME2ZHWBOPRSXJ",
+            "Is6HGlvzFH/39QtCps2sBZ0FLIqt3YPguiIhaos+"
+    );
+
+    AmazonS3 s3client = AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(Regions.EU_WEST_2)
+            .build();
+
+    public fileStorageBean() {
+        System.out.println("init");
     }
+
+    public boolean  createBucket(String bucketName) {
+        if(!s3client.doesBucketExistV2(bucketName)) {
+            s3client.createBucket(bucketName);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Bucket> getBuckets(){
+        return s3client.listBuckets();
+    }
+
+    public boolean deleteBucket(String bucketName) {
+        try {
+            s3client.deleteBucket(bucketName);
+            return true;
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            return false;
+        }
+    }
+
+    public String storeObject(InputStream inputStream, String bucketName, String fileName) {
+        s3client.putObject(
+                bucketName,
+                fileName,
+                inputStream,
+                new ObjectMetadata()
+        );
+        return fileName;
+    }
+
 }
