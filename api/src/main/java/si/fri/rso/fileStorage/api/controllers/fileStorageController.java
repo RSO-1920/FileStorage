c3f53a5f555c5456a18aa249054ac621881b4ce7
dp <david.penca111@gmail.com>
2019-12-10 04:54:14 +0100

    OpenApi on http://localhost:8081/api-specs/v1/openapi.json

--- api/src/main/java/si/fri/rso/fileStorage/api/controllers/fileStorageController.java
+++ api/src/main/java/si/fri/rso/fileStorage/api/controllers/fileStorageController.java
@@ -5,6 +5,11 @@ import javax.ws.rs.PathParam;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.glassfish.jersey.media.multipart.MultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
@@ -19,8 +24,10 @@ import si.fri.rso.fileStorage.services.fileStorageBean;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.io.InputStream;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -37,12 +44,33 @@ public class fileStorageController {
     @Inject
     private FileStorageConfigProperties fileStorageConfigProperties;
 
+    @Context
+    protected UriInfo uriInfo;
+
+    @Operation(description = "Returns list all AWS S3 buckets.", summary = "Bucket list", tags = "buckets", responses = {
+            @ApiResponse(responseCode = "200",
+                    description = "List of buckets",
+                    content = @Content(
+                            array = @ArraySchema(schema = @Schema(implementation = Bucket.class)))
+            )
+    })
     @GET
-    public Response getFile() {
+    public Response getBuckets() {
         List<Bucket> buckets = fileStorage.listBuckets();
         return Response.status(Response.Status.OK).entity(buckets).build();
     }
 
+    @Operation(description = "Returns list all files in the given AWS S3 bucket.", summary = "File list", tags = "files", responses = {
+            @ApiResponse(responseCode = "200",
+                    description = "List of files",
+                    content = @Content(
+                            array = @ArraySchema(schema = @Schema(implementation = ObjectListing.class)))
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "Bucket not found",
+                    content = @Content(schema = @Schema(implementation = Error.class))
+            )
+    })
     @GET
     @Path("{bucketName}")
     public Response getFile(@PathParam("bucketName") String bucketName) {
@@ -53,6 +81,12 @@ public class fileStorageController {
             return Response.status(Response.Status.OK).entity(files).build();
     }
 
+    @Operation(description = "Creates an AWS S3 bucket with the given name.", summary = "Create bucket", tags = "create, bucket", responses = {
+            @ApiResponse(responseCode = "200",
+                    description = "Created bucket",
+                    content = @Content(schema = @Schema(implementation = Bucket.class))
+            )
+    })
     @POST
     @Path("{bucketName}")
     public Response createBucket(@PathParam("bucketName") String bucketName) {
@@ -60,7 +94,16 @@ public class fileStorageController {
         return Response.status(Response.Status.OK).entity(createdBucket).build();
     }
 
-
+    @Operation(description = "Deletes the specified AWS S3 bucket.", summary = "Delete bucket", tags = "delete, bucket", responses = {
+            @ApiResponse(responseCode = "200",
+                    description = "Deleted file",
+                    content = @Content(schema = @Schema(implementation = String.class))
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "File not found",
+                    content = @Content(schema = @Schema(implementation = Error.class))
+            )
+    })
     @DELETE
     @Path("{bucketName}")
     public Response deleteBucket(@PathParam("bucketName") String bucketName) {
@@ -68,21 +111,33 @@ public class fileStorageController {
         if (bucketeDeleated)
             return Response.status(Response.Status.OK).entity("Deleted bucket " + bucketName).build();
 
-        return Response.status(Response.Status.OK).entity("Failed to delete bucket " + bucketName +"!").build();
+        return Response.status(Response.Status.NOT_FOUND).entity("Failed to delete bucket " + bucketName +"!").build();
     }
 
+    @Operation(description = "Get the specified file from AWS S3 bucket.", summary = "Get file", tags = "get, file", responses = {
+            @ApiResponse(responseCode = "200",
+                    description = "File",
+                    content = @Content(schema = @Schema(implementation = S3ObjectInputStream.class))
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "File not found",
+                    content = @Content(schema = @Schema(implementation = Error.class))
+            )
+    })
     @GET
     @Path("{bucketName}/{fileName}")
     @Produces(MediaType.MULTIPART_FORM_DATA)
     public Response downloadFile(@PathParam("bucketName") String bucketName, @PathParam("fileName") String fileName) {
         S3ObjectInputStream outputStream = fileStorage.downloadFile(bucketName, fileName);
-
-        /*final MultiPart multiPart = new MultiPart();
-        multiPart.bodyPart(new StreamDataBodyPart("fileStream", outputStream));
-        return Response.status(Response.Status.OK).entity(multiPart).build();*/
-        return Response.status(Response.Status.OK).entity(outputStream).build();
+        return Response.status(outputStream == null ? Response.Status.NOT_FOUND : Response.Status.OK).entity(outputStream == null ? "Error: File " + bucketName + "/" + fileName + " not found" : outputStream).build();
     }
 
+    @Operation(description = "Upload file to AWS S3 bucket.", summary = "Upload file", tags = "upload, file", responses = {
+            @ApiResponse(responseCode = "200",
+                    description = "Confirmation",
+                    content = @Content(schema = @Schema(implementation = String.class))
+            )
+    })
     @POST
     @Path("{bucketName}/{fileName}")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -96,11 +151,21 @@ public class fileStorageController {
         return Response.status(Response.Status.OK).entity("ok").build();
     }
 
+    @Operation(description = "Delete the specified file from AWS S3 bucket.", summary = "Delete file", tags = "delete, file", responses = {
+            @ApiResponse(responseCode = "200",
+                    description = "Confirmation",
+                    content = @Content(schema = @Schema(implementation = String.class))
+            ),
+            @ApiResponse(responseCode = "404",
+                    description = "File not found",
+                    content = @Content(schema = @Schema(implementation = Error.class))
+            )
+    })
     @DELETE
     @Path("{bucketName}/{fileName}")
     public Response deleteFile(@PathParam("bucketName") String bucketName, @PathParam("fileName") String fileName) {
         boolean deleted = fileStorage.deleteFile(bucketName, fileName);
-            return Response.status(Response.Status.OK).entity("file " + bucketName + "/" + fileName + " deleted: " + (deleted ? "successfully" : "unsuccessfully")).build();
+            return Response.status(deleted ? Response.Status.OK : Response.Status.NOT_FOUND).entity("file " + bucketName + "/" + fileName + " deleted: " + (deleted ? "successfully" : "unsuccessfully")).build();
     }
 
 }
--- api/src/main/java/si/fri/rso/fileStorage/api/fileStorageApi.java
+++ api/src/main/java/si/fri/rso/fileStorage/api/fileStorageApi.java
@@ -1,6 +1,11 @@
 package si.fri.rso.fileStorage.api;
 
 import com.kumuluz.ee.discovery.annotations.RegisterService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -12,6 +17,7 @@ import java.util.Map;
 import java.util.Set;
 
 @RegisterService(value = "rso1920-fileStorage")
+@OpenAPIDefinition(info = @Info(title = "FileStorage Rest API", version = "v1", contact = @Contact(), license = @License(), description = "File storage API that comunicates with AWS S3"), servers = @Server(url ="http://localhost:8081/v1"))
 @ApplicationPath("/v1")
 public class fileStorageApi extends Application {
     @Override
