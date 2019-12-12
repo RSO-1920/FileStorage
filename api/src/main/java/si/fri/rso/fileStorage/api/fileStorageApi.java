7b1f538bc467aab8e30fc19fe2bedb3f680525ed
aa <aa>
2019-12-12 10:38:40 +0100

    Added simple healthchecks TODO: Add healthcheck for AWS S3.

--- api/src/main/java/si/fri/rso/fileStorage/api/fileStorageApi.java
+++ api/src/main/java/si/fri/rso/fileStorage/api/fileStorageApi.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.Set;
 
 @RegisterService(value = "rso1920-fileStorage")
-@OpenAPIDefinition(info = @Info(title = "FileStorage Rest API", version = "v1", contact = @Contact(), license = @License(), description = "File storage API that comunicates with AWS S3"), servers = @Server(url ="http://localhost:8081/v1"))
+@OpenAPIDefinition(info = @Info(title = "FileStorage Rest API", version = "v1", contact = @Contact(), license = @License(), description = "File storage API that communicates with AWS S3"), servers = @Server(url ="http://localhost:8081/v1"))
 @ApplicationPath("/v1")
 public class fileStorageApi extends Application {
     @Override
